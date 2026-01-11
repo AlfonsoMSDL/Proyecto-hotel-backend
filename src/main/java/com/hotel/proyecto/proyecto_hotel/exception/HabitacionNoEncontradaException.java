25321be87c1038b152ff38717dcdf9b0dd6a52da
AlfonsoMSDL <alfonsomanuelsalazardeleon49@gmail.com>
2026-01-11 13:44:12 -0500

    Renombrando excepcion

---
+++ src/main/java/com/hotel/proyecto/proyecto_hotel/exception/HabitacionNoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.hotel.proyecto.proyecto_hotel.exception;
+
+public class HabitacionNoEncontradaException extends RuntimeException {
+    public HabitacionNoEncontradaException(String mensaje) {
+        super(mensaje);
+    }
+}
