d546969d90aca1eda24df2bf0a941ad4cea9756d
AlfonsoMSDL <alfonsomanuelsalazardeleon49@gmail.com>
2026-01-19 10:56:48 -0500

    Agregando anotacion a la interfaz de repositorio

--- src/main/java/com/hotel/proyecto/proyecto_hotel/repository/EstadoDeLaReservaRepository.java
+++ src/main/java/com/hotel/proyecto/proyecto_hotel/repository/EstadoDeLaReservaRepository.java
@@ -2,6 +2,8 @@ package com.hotel.proyecto.proyecto_hotel.repository;
 
 import com.hotel.proyecto.proyecto_hotel.model.EstadoDeLaReserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstadoDeLaReservaRepository extends JpaRepository<EstadoDeLaReserva, Long> {
 }
